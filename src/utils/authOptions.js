435c0be1aa9a7d1d2174673d3a745105680d814b
Trehan <tsumiksh@gmail.com>
2026-01-06 16:59:43 -0500

    login to prompt for google id

--- src/utils/authOptions.js
+++ src/utils/authOptions.js
@@ -5,6 +5,13 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account", // This forces the "Choose an account" screen
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
